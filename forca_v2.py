d8fdbf3bbaa71476ee0f20f345501e0b618e842e
Gabriela Coelho <87398488+coelhos-gabi@users.noreply.github.com>
2022-01-17 19:58:50 -0300

    Add files via upload

---
+++ forca_v2.py
@@ -0,0 +1,192 @@
+# Hangman Game (Jogo da Forca)
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Board (tabuleiro)
+board = ['''
+
+>>>>>>>>>>Jogo da Forca<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+# Classe
+class Hangman:
+
+    # Método Construtor
+    def __init__(self, word):
+        self.word = word
+        self.letras_erradas = []
+        self.letras_certas = []
+        self.palavra_secreta = []
+
+    # Método para adivinhar a letra
+    def check(self, chute):
+        index = 0
+        contador = 0
+        for letra in self.word:
+            if chute == letra:
+                self.palavra_secreta[index] = chute
+                self.letras_certas.append(letra)
+            else:
+                contador += 1
+            index += 1
+        if contador == len(self.word):
+            self.letras_erradas.append(chute)
+            del(board[0])
+
+
+    # Método para verificar se o jogo terminou
+    def hangman_over(self):
+        if self.hangman_won() == True:
+            return True
+        elif len(board) == 1:
+            return True
+        else:
+            return False
+
+    # Método para verificar se o jogador venceu
+
+    def hangman_won(self):
+        contador = 0
+        acertou = False
+        for elemento in self.letras_certas:
+            if elemento in self.palavra_secreta:
+                contador += 1
+        if contador == len(self.palavra_secreta):
+            acertou = True
+        return acertou
+
+    # Método para não mostrar a letra no board
+
+    def hide_word(self):
+        for letra in self.word:
+            self.palavra_secreta.append("-")
+        for elemento in self.palavra_secreta:
+            print(elemento, end='')
+
+    # Método para checar o status do game e imprimir o board na tela
+    def print_game_status(self):
+        print(board[0])
+        print('Palavra secreta:')
+        for letra in self.palavra_secreta:
+            print(letra,end='')
+        print('\n')
+        print('Letras certas:')
+        for letra in sorted(set(self.letras_certas)):
+            print(letra, end='')
+        print('\n')
+        print('Letras erradas:')
+        for letra in sorted(set(self.letras_erradas)):
+            print(letra, end='')
+        print('\n')
+        
+
+
+# Método para ler uma palavra de forma aleatória do banco de palavras
+def rand_word(categoria):
+    if categoria == 1: 
+        with open("animais.txt", "rt") as f:
+            bank = f.readlines()
+        return bank[random.randint(0, len(bank))].strip()
+    elif categoria == 2: 
+        with open("objetos.txt", "rt") as f:
+            bank = f.readlines()
+        return bank[random.randint(0, len(bank))].strip()
+    else: 
+        with open("frutas.txt", "rt") as f:
+            bank = f.readlines()
+        return bank[random.randint(0, len(bank))].strip()
+
+# Método Main - Execução do Programa
+
+
+def main():
+
+    # Objeto
+    
+    print('*********************************')
+    print('***Bem vindo ao jogo da Forca!***')
+    print('*********************************')
+    print('\n\nQual categoria deseja jogar?')
+    categoria = int(input('1 - Animais\n2 - Objetos\n3 - Frutas\nCategoria: '))
+    game = Hangman(rand_word(categoria))
+    print(board[0])
+    print('Palavra secreta: ')
+    game.hide_word()
+    print('\n')
+
+    while True:
+        chute = input('Tente uma letra: ').lower().strip()
+        game.check(chute)
+        game.print_game_status()
+        acertou = game.hangman_won()
+        acabou = game.hangman_over()
+        if acertou == True:
+            print('Você ganhou o jogo!')
+            break
+        if acabou == True:
+            print('Fim de jogo!')
+            break
+    
+    print(f'A palavra era: {game.word.capitalize()}')
+
+# Executa o programa
+if __name__ == "__main__":
+    main()
